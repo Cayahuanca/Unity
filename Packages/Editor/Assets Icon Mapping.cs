a85894e1b5a5351b47cbd429fb3449f20af1319d
Praecipua <git@mail.praecipua.org>
2023-11-21 04:46:34 +0900

    Add Assets Icon that attach icons to folders in the Project window.
    Project ウィンドウで、フォルダにアイコンをつける機能を実装。

---
+++ Packages/Editor/Assets Icon Mapping.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Praecipua.EE
+{
+    [CreateAssetMenu(fileName = "AssetsIconMapping", menuName = "Custom/AssetsIcon Mapping")]
+    public class AssetsIconMapping : ScriptableObject
+    {
+        [System.Serializable]
+        public struct IconMapping
+        {
+            public string guid;
+            public Texture2D icon;
+        }
+
+        public IconMapping[] iconMappings;
+    }
+}
---
+++ Packages/Editor/Assets Icon.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+
+namespace Praecipua.EE
+{
+    [InitializeOnLoad]
+    public class AssetsIconDraw
+    {
+        static AssetsIconDraw()
+        {
+            EditorApplication.projectWindowItemOnGUI += ProjectWindowItemOnGUI;
+        }
+
+        private static void ProjectWindowItemOnGUI(string guid, Rect selectionRect)
+        {
+            // AssetsIconMapping.asset をロード
+            AssetsIconMapping iconMapping = AssetDatabase.LoadAssetAtPath<AssetsIconMapping>("Assets/Praecipua/AssetsIconMapping.asset");
+
+            // アイコンをマッピングから検索
+            Texture2D icon = null;
+            if (iconMapping != null && iconMapping.iconMappings != null)
+            {
+                foreach (var mapping in iconMapping.iconMappings)
+                {
+                    if (mapping.guid == guid)
+                    {
+                        icon = mapping.icon;
+                        break;
+                    }
+                }
+            }
+
+            // アイコンを描画
+            if (icon != null)
+            {
+                float iconMaxSize = selectionRect.height;
+                float iconBackSize = iconMaxSize;
+                float iconSize = iconMaxSize;
+                if (iconMaxSize > 32)
+                {
+                    iconSize = iconBackSize - 16;
+                }
+                if (selectionRect.x == 14)
+                {
+                    selectionRect.x = selectionRect.x + 2;
+                }
+
+                // アイコンのサイズを表示サイズに合わせて調整
+                Rect iconRect = new Rect(selectionRect.x, selectionRect.y, iconSize, iconSize);
+                GUI.DrawTexture(iconRect, icon);
+            }
+        }
+    }
+}
